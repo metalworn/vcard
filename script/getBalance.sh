2c6a4cd824549412405d3d44f02608987a4ade8e
metalworn <118116430+metalworn@users.noreply.github.com>
2020-06-26 06:35:27 +0000

    Update exec-committee.csv

--- script/getBalance.sh
+++ script/getBalance.sh
@@ -4,4 +4,4 @@
 # Define the ListAddresses into variable and format to simple output
 address=`curl --silent -XGET http://127.0.0.1:5359/api/ListAddresses | jq '.addresses[0]' |cut -d '"' -f 2`
 
-curl --silent -X
+curl --silent -XPOST http://127.0.0.1:5359/api/GetBalance -d '{"address": "'$address'"}' |jq '.balance' | rev |sed 's/./&./10' |rev
