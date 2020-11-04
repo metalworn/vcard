877a675e41ba0f79211a8085ab8b8ac2c4e8974b
metalworn <118116430+metalworn@users.noreply.github.com>
2020-11-04 22:00:08 +0000

    learning

---
+++ web/index.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <html class="no-js" lang="en">
+  <meta name="description" content="QRL Faucet">
+  <meta name="keywords" content="QRL, Faucet,  Free, Crypto Currency, Quantum Resistant Ledger">
+  <meta name="author" content="fr1t2">
+  <title>QRL Faucet</title>
+  <link rel="stylesheet" href="/css/foundation.css">
+  <link rel="stylesheet" href="/css/app.css">
+  <link rel="shortcut icon" href="/assets/favicon.png" type="image/x-icon" />
+</head>
+<body>
+  <div class="grid-container full ">
+    <!-- Small section -->
+    <div class="off-canvas position-left hide-for-medium" id="offCanvasLeft" data-off-canvas>
+      <!-- Menu -->
