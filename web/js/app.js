d356df03234a52fd81e36a801dce67d2beff9f53
metalworn <118116430+metalworn@users.noreply.github.com>
2020-11-05 07:16:23 +0000

    Create introduction.md

--- web/js/app.js
+++ web/js/app.js
@@ -1,3 +1,13 @@
 function myCustomValidator(
   $el,      /* jQuery element to validate */
-  required, /* is the element required according to the `[required]` attrib
+  required, /* is the element required according to the `[required]` attribute */
+  parent    /* parent of the jQuery element `$el` */
+) {
+  if (!required) return true;
+  var from = $('#'+$el.attr('data-greater-than')).val(),
+      to = $el.val();
+  return (parseInt(to) > parseInt(from));
+}
+
+// Set default options
+Foundation.Abide.defaults.patterns['qrl_address'] = /^(Q|q)[0-9a-zA-Z]{7
