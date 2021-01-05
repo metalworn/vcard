8348065ac6a3cf6418a9ce733fdfef09ebc7a285
metalworn <118116430+metalworn@users.noreply.github.com>
2021-01-05 18:42:22 +0000

    Prepare Pexip packaging.

--- web/js/vendor/foundation.js
+++ web/js/vendor/foundation.js
@@ -6159,3 +6159,296 @@ var Equalizer = function (_Plugin) {
   }, {
     key: '_isStacked',
     value: function _isStacked() {
+      if (!this.$watched[0] || !this.$watched[1]) {
+        return true;
+      }
+      return this.$watched[0].getBoundingClientRect().top !== this.$watched[1].getBoundingClientRect().top;
+    }
+
+    /**
+     * Finds the outer heights of children contained within an Equalizer parent and returns them in an array
+     * @param {Function} cb - A non-optional callback to return the heights array to.
+     * @returns {Array} heights - An array of heights of children within Equalizer container
+     */
+
+  }, {
+    key: 'getHeights',
+    value: function getHeights(cb) {
+      var heights = [];
+      for (var i = 0, len = this.$watched.length; i < len; i++) {
+        this.$watched[i].style.height = 'auto';
+        heights.push(this.$watched[i].offsetHeight);
+      }
+      cb(heights);
+    }
+
+    /**
+     * Finds the outer heights of children contained within an Equalizer parent and returns them in an array
+     * @param {Function} cb - A non-optional callback to return the heights array to.
+     * @returns {Array} groups - An array of heights of children within Equalizer container grouped by row with element,height and max as last child
+     */
+
+  }, {
+    key: 'getHeightsByRow',
+    value: function getHeightsByRow(cb) {
+      var lastElTopOffset = this.$watched.length ? this.$watched.first().offset().top : 0,
+          groups = [],
+          group = 0;
+      //group by Row
+      groups[group] = [];
+      for (var i = 0, len = this.$watched.length; i < len; i++) {
+        this.$watched[i].style.height = 'auto';
+        //maybe could use this.$watched[i].offsetTop
+        var elOffsetTop = __WEBPACK_IMPORTED_MODULE_0_jquery___default()(this.$watched[i]).offset().top;
+        if (elOffsetTop != lastElTopOffset) {
+          group++;
+          groups[group] = [];
+          lastElTopOffset = elOffsetTop;
+        }
+        groups[group].push([this.$watched[i], this.$watched[i].offsetHeight]);
+      }
+
+      for (var j = 0, ln = groups.length; j < ln; j++) {
+        var heights = __WEBPACK_IMPORTED_MODULE_0_jquery___default()(groups[j]).map(function () {
+          return this[1];
+        }).get();
+        var max = Math.max.apply(null, heights);
+        groups[j].push(max);
+      }
+      cb(groups);
+    }
+
+    /**
+     * Changes the CSS height property of each child in an Equalizer parent to match the tallest
+     * @param {array} heights - An array of heights of children within Equalizer container
+     * @fires Equalizer#preequalized
+     * @fires Equalizer#postequalized
+     */
+
+  }, {
+    key: 'applyHeight',
+    value: function applyHeight(heights) {
+      var max = Math.max.apply(null, heights);
+      /**
+       * Fires before the heights are applied
+       * @event Equalizer#preequalized
+       */
+      this.$element.trigger('preequalized.zf.equalizer');
+
+      this.$watched.css('height', max);
+
+      /**
+       * Fires when the heights have been applied
+       * @event Equalizer#postequalized
+       */
+      this.$element.trigger('postequalized.zf.equalizer');
+    }
+
+    /**
+     * Changes the CSS height property of each child in an Equalizer parent to match the tallest by row
+     * @param {array} groups - An array of heights of children within Equalizer container grouped by row with element,height and max as last child
+     * @fires Equalizer#preequalized
+     * @fires Equalizer#preequalizedrow
+     * @fires Equalizer#postequalizedrow
+     * @fires Equalizer#postequalized
+     */
+
+  }, {
+    key: 'applyHeightByRow',
+    value: function applyHeightByRow(groups) {
+      /**
+       * Fires before the heights are applied
+       */
+      this.$element.trigger('preequalized.zf.equalizer');
+      for (var i = 0, len = groups.length; i < len; i++) {
+        var groupsILength = groups[i].length,
+            max = groups[i][groupsILength - 1];
+        if (groupsILength <= 2) {
+          __WEBPACK_IMPORTED_MODULE_0_jquery___default()(groups[i][0][0]).css({ 'height': 'auto' });
+          continue;
+        }
+        /**
+          * Fires before the heights per row are applied
+          * @event Equalizer#preequalizedrow
+          */
+        this.$element.trigger('preequalizedrow.zf.equalizer');
+        for (var j = 0, lenJ = groupsILength - 1; j < lenJ; j++) {
+          __WEBPACK_IMPORTED_MODULE_0_jquery___default()(groups[i][j][0]).css({ 'height': max });
+        }
+        /**
+          * Fires when the heights per row have been applied
+          * @event Equalizer#postequalizedrow
+          */
+        this.$element.trigger('postequalizedrow.zf.equalizer');
+      }
+      /**
+       * Fires when the heights have been applied
+       */
+      this.$element.trigger('postequalized.zf.equalizer');
+    }
+
+    /**
+     * Destroys an instance of Equalizer.
+     * @function
+     */
+
+  }, {
+    key: '_destroy',
+    value: function _destroy() {
+      this._pauseEvents();
+      this.$watched.css('height', 'auto');
+    }
+  }]);
+
+  return Equalizer;
+}(__WEBPACK_IMPORTED_MODULE_4__foundation_plugin__["a" /* Plugin */]);
+
+/**
+ * Default settings for plugin
+ */
+
+
+Equalizer.defaults = {
+  /**
+   * Enable height equalization when stacked on smaller screens.
+   * @option
+   * @type {boolean}
+   * @default false
+   */
+  equalizeOnStack: false,
+  /**
+   * Enable height equalization row by row.
+   * @option
+   * @type {boolean}
+   * @default false
+   */
+  equalizeByRow: false,
+  /**
+   * String representing the minimum breakpoint size the plugin should equalize heights on.
+   * @option
+   * @type {string}
+   * @default ''
+   */
+  equalizeOn: ''
+};
+
+
+
+/***/ }),
+/* 22 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Interchange; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__foundation_util_mediaQuery__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__foundation_plugin__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__foundation_util_core__ = __webpack_require__(1);
+
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+
+
+
+
+
+/**
+ * Interchange module.
+ * @module foundation.interchange
+ * @requires foundation.util.mediaQuery
+ */
+
+var Interchange = function (_Plugin) {
+  _inherits(Interchange, _Plugin);
+
+  function Interchange() {
+    _classCallCheck(this, Interchange);
+
+    return _possibleConstructorReturn(this, (Interchange.__proto__ || Object.getPrototypeOf(Interchange)).apply(this, arguments));
+  }
+
+  _createClass(Interchange, [{
+    key: '_setup',
+
+    /**
+     * Creates a new instance of Interchange.
+     * @class
+     * @name Interchange
+     * @fires Interchange#init
+     * @param {Object} element - jQuery object to add the trigger to.
+     * @param {Object} options - Overrides to the default plugin settings.
+     */
+    value: function _setup(element, options) {
+      this.$element = element;
+      this.options = __WEBPACK_IMPORTED_MODULE_0_jquery___default.a.extend({}, Interchange.defaults, options);
+      this.rules = [];
+      this.currentPath = '';
+      this.className = 'Interchange'; // ie9 back compat
+
+      this._init();
+      this._events();
+    }
+
+    /**
+     * Initializes the Interchange plugin and calls functions to get interchange functioning on load.
+     * @function
+     * @private
+     */
+
+  }, {
+    key: '_init',
+    value: function _init() {
+      __WEBPACK_IMPORTED_MODULE_1__foundation_util_mediaQuery__["a" /* MediaQuery */]._init();
+
+      var id = this.$element[0].id || __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_3__foundation_util_core__["a" /* GetYoDigits */])(6, 'interchange');
+      this.$element.attr({
+        'data-resize': id,
+        'id': id
+      });
+
+      this._addBreakpoints();
+      this._generateRules();
+      this._reflow();
+    }
+
+    /**
+     * Initializes events for Interchange.
+     * @function
+     * @private
+     */
+
+  }, {
+    key: '_events',
+    value: function _events() {
+      var _this3 = this;
+
+      this.$element.off('resizeme.zf.trigger').on('resizeme.zf.trigger', function () {
+        return _this3._reflow();
+      });
+    }
+
+    /**
+     * Calls necessary functions to update Interchange upon DOM change
+     * @function
+     * @private
+     */
+
+  }, {
+    key: '_reflow',
+    value: function _reflow() {
+      var match;
+
+      // Iterate through each rule, but only save the last match
+      for (var i in this.rules) {
+        if (this.rules.hasOwnProperty(i)) {
+          var rule = this.rules[i];
+          if (window.matchMedia(rule.query).matches) {
+            match = rule;
