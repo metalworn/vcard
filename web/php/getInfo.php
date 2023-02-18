0e01d6c4bd1244dd771029a6ba525e1be6eca321
metalworn <118116430+metalworn@users.noreply.github.com>
2023-02-18 12:54:46 +0000

    Add more integration tests

--- web/php/getInfo.php
+++ web/php/getInfo.php
@@ -2,4 +2,16 @@
 
 /**
 * This script grabs the IP address and validates it as a valid address
-* If it shows a proxy or is on a private network it os rejected and
+* If it shows a proxy or is on a private network it os rejected and 
+*
+*
+**/
+
+function get_ip_address() {
+    // check for shared internet/ISP IP
+    if (!empty($_SERVER['HTTP_CLIENT_IP']) && validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    }
+    // check for IPs passing through proxies
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // check 
